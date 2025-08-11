52bc664f16df4db1bd63b7eff55e0b5331f09a40
Elmer Mamani <elmer.mamani.urrelo@gmail.com>
2025-08-10 20:58:54 -0400

    First Commit

---
+++ Main.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Lolchat_offline
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            btnActive = new Button();
+            SuspendLayout();
+            // 
+            // btnActive
+            // 
+            btnActive.Location = new Point(89, 66);
+            btnActive.Name = "btnActive";
+            btnActive.Size = new Size(112, 44);
+            btnActive.TabIndex = 0;
+            btnActive.Text = "Activar";
+            btnActive.UseVisualStyleBackColor = true;
+            btnActive.Click += btnActive_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(296, 195);
+            Controls.Add(btnActive);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MaximumSize = new Size(312, 234);
+            MinimumSize = new Size(312, 234);
+            Name = "Form1";
+            SizeGripStyle = SizeGripStyle.Hide;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Lolchat Offline";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnActive;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetFwTypeLib;
+
+namespace Lolchat_offline
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            if (GetFirewallRuleByName("lolchat") == null)
+            {
+                CreateRuleLolchat();
+            }
+
+            Thread.Sleep(2000);
+            InitializeComponent();
+
+            try
+            {
+                INetFwRule lolchatRule = GetFirewallRuleByName("lolchat");
+
+                btnActive.Text = lolchatRule.Enabled ? "Desactivar" : "Activar";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se debe ejecutar como administrador.");
+            }
+        }
+
+        private void btnActive_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                INetFwRule lolchatRule = GetFirewallRuleByName("lolchat");
+
+                lolchatRule.Enabled = !lolchatRule.Enabled;
+                btnActive.Text = lolchatRule.Enabled ? "Desactivar" : "Activar";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se debe ejecutar como administrador.");
+            }
+
+        }
+ 
+
+        private INetFwRule GetFirewallRuleByName(string ruleName)
+        {
+            try
+            {
+                // Crear una instancia de la política del firewall
+                Type netFwPolicy2Type = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+                INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(netFwPolicy2Type);
+
+                // Obtener la colección de reglas
+                INetFwRules fwRules = fwPolicy2.Rules;
+
+                // Iterar sobre las reglas para encontrar la que coincide
+                foreach (INetFwRule rule in fwRules)
+                {
+                    if (rule.Name == ruleName)
+                    {
+                        return rule; // Retorna la regla si la encuentra
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se debe ejecutar como administrador.");
+            }
+            return null; // Retorna null si no se encontró la regla
+        }
+
+        private void CreateRuleLolchat()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+
+            string comando = "netsh advfirewall firewall add rule name=\"lolchat\" dir=out remoteport=5223 protocol=TCP action=block enable=no";
+            process.StartInfo.Arguments =  $"/C {comando}";
+
+            try
+            {
+                process.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al crear regla.");
+            }
+        }
+    }
+}
